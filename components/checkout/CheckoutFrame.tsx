3628eb25c15ca852075b2815c1e9d604b2842b31
Raj Varsani <varasaniraj5@gmail.com>
2023-12-18 01:31:49 +0530

    feat: add presistent storage of cart items - provide feature to repopulate the order data

--- components/checkout/CheckoutFrame.tsx
+++ components/checkout/CheckoutFrame.tsx
@@ -1,17 +1,30 @@
-import { ActionIcon, Container, Flex } from "@mantine/core";
-import { IconChevronLeft } from "@tabler/icons-react";
+import { useCheckoutStore } from "@/store/checkout.store";
+import { ActionIcon, Container, Flex, Tooltip } from "@mantine/core";
+import { IconChevronLeft, IconRefresh } from "@tabler/icons-react";
 import styles from "./CheckoutFrame.module.css";
 
 function CheckoutFrame({ children }: { children: any }) {
+  const refreshOrderDetails = useCheckoutStore(
+    (state) => state.refreshOrderDetails
+  );
   return (
     <Container className={styles.checkoutFrame} px={20}>
-      <Flex py={16}>
+      <Flex py={16} justify="space-between">
         <ActionIcon aria-label="Back">
           <IconChevronLeft
             style={{ width: "70%", height: "70%" }}
             stroke={1.5}
           />
         </ActionIcon>
+        <Tooltip label="Repopulate Order Data">
+          <ActionIcon
+            aria-label="Back"
+            onClick={() => refreshOrderDetails(true)}
+            variant="transparent"
+          >
+            <IconRefresh style={{ width: "70%", height: "70%" }} stroke={1.5} />
+          </ActionIcon>
+        </Tooltip>
       </Flex>
       <Flex direction="column" h="100%">
         {children}
--- store/checkout.store.ts
+++ store/checkout.store.ts
@@ -5,6 +5,7 @@ import {
 } from "@/interfaces/ICheckout";
 import { getOrderDetails } from "@/services/checkout.service";
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 
 type Store = {
   cartItems: ICartProduct[];
@@ -12,7 +13,7 @@ type Store = {
   selectedPaymentMethod: PaymentMethod | null;
   loading: boolean;
   error: string | null;
-  refreshOrderDetails: () => Promise<void>;
+  refreshOrderDetails: (forced?: boolean) => void;
   updateCartItems: (cartItems: ICartProduct[]) => void;
   updatePaymentMethod: (paymentMethod: PaymentMethod) => void;
   promoCode: {
@@ -23,43 +24,50 @@ type Store = {
   removePromoCode: () => void;
 };
 
-export const useCheckoutStore = create<Store>((set, get) => ({
-  cartItems: [],
-  availablePaymentMethods: [],
-  selectedPaymentMethod: null,
-  loading: true,
-  error: null,
-  promoCode: null,
-  refreshOrderDetails: async () => {
-    // if (get().loading) return;
-    set({ loading: true });
-    try {
-      const data: IOrderDetails = (await getOrderDetails()).data;
-      set({
-        cartItems: data.products,
-        availablePaymentMethods: data.paymentMethods,
-        selectedPaymentMethod: data.paymentMethods[0],
-        loading: false,
-      });
-    } catch (e: any) {
-      set({ error: e, loading: false });
-    }
-  },
-  updateCartItems: (cartItems: ICartProduct[]) => {
-    set({ cartItems });
-  },
-  updatePaymentMethod: (paymentMethod: PaymentMethod) => {
-    set({ selectedPaymentMethod: paymentMethod });
-  },
-  applyPromoCode: () => {
-    set({
-      promoCode: {
-        name: "GROWW",
-        getDiscount: (total: number) => Math.min(total * 0.1, 10),
+export const useCheckoutStore = create(
+  persist<Store>(
+    (set, get) => ({
+      cartItems: [],
+      availablePaymentMethods: [],
+      selectedPaymentMethod: null,
+      loading: true,
+      error: null,
+      promoCode: null,
+      refreshOrderDetails: async (forced: boolean = false) => {
+        if (!forced && get().cartItems.length > 0) return;
+        set({ loading: true });
+        try {
+          const data: IOrderDetails = (await getOrderDetails()).data;
+          set({
+            cartItems: data.products,
+            availablePaymentMethods: data.paymentMethods,
+            selectedPaymentMethod: data.paymentMethods[0],
+            loading: false,
+          });
+        } catch (e: any) {
+          set({ error: e, loading: false });
+        }
       },
-    });
-  },
-  removePromoCode: () => {
-    set({ promoCode: null });
-  },
-}));
+      updateCartItems: (cartItems: ICartProduct[]) => {
+        set({ cartItems });
+      },
+      updatePaymentMethod: (paymentMethod: PaymentMethod) => {
+        set({ selectedPaymentMethod: paymentMethod });
+      },
+      applyPromoCode: () => {
+        set({
+          promoCode: {
+            name: "GROWW",
+            getDiscount: (total: number) => Math.min(total * 0.1, 10),
+          },
+        });
+      },
+      removePromoCode: () => {
+        set({ promoCode: null });
+      },
+    }),
+    {
+      name: "cart-store",
+    }
+  )
+);
